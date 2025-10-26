404e4fe7dc548488f8c7eda04e455a8199f27476
Nirob <nirobsarker012@gmail.com>
2025-10-26 18:02:35 +0600

    add: pricecard-section

---
+++ app/components/Price/PriceCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PriceCard = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-3 justify-between items-center"></div>
+    </section>
+  );
+};
+
+export default PriceCard;
